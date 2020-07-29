fc388de4d3490d25416423f8515ab439435063db
swfarnsworth <swfarnsworth@gmail.com>
2020-07-29 13:41:46 -0400

    Need to figure out why there are repetitions

--- create_rels.py
+++ create_rels.py
@@ -1,8 +1,7 @@
 import argparse
 import typing as t
-from copy import copy
-from dataclasses import dataclass
-from enum import Enum
+from collections import namedtuple
+from copy import deepcopy
 
 import spacy
 import torch
@@ -10,26 +9,12 @@ import transformers as tfs
 from bratlib import data as brat_data
 from spacy.tokens.span import Span
 
-
 nlp = spacy.load('en_core_sci_lg')
 bert = tfs.BertForMaskedLM.from_pretrained('allenai/scibert_scivocab_uncased')
 bert_tokenizer = tfs.BertTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
 
-
-@dataclass
-class PseudoSentence:
-    rel: brat_data.Relation
-    sent: str
-
-
+PseudoSentence = namedtuple('PseudoSentence', 'rel sent')
 SentenceGenerator = t.Generator[PseudoSentence, None, None]
-
-
-def adjust_ent_span(ent: brat_data.Entity, offset: int) -> None:
-    start, end = ent.spans[0]
-    start += offset
-    end += offset
-    ent.spans = [(start, end)]
 
 
 def find_sentence(start: int, sentences: t.Dict[range, Span]):
@@ -39,101 +24,66 @@ def find_sentence(start: int, sentences: t.Dict[range, Span]):
     raise RuntimeError(f'start value {start} not in any range')
 
 
-_Side = Enum('RIGHT LEFT')
+def _pseudofy_side(rel: brat_data.Relation, sentence: Span, k: int, do_left=True) -> SentenceGenerator:
+    rel = deepcopy(rel)
 
-def _pseudofy_side(rel: brat_data.Relation, side: _Side):
-    ent = rel.arg1 if side is _Side.RIGHT else rel.arg2
+    if do_left:
+        ent, other_ent = rel.arg1, rel.arg2
+    else:
+        other_ent, ent = rel.arg1, rel.arg2
 
-
-
-def pseudofy_relation(rel: brat_data.Relation, sentence: Span, k=3) -> SentenceGenerator:
     text = str(sentence)
     span_start = sentence.start_char
 
-    arg1_start = rel.arg1.spans[0][0] - span_start
-    arg1_end = rel.arg1.spans[0][-1] - span_start
+    start = ent.spans[0][0] - span_start
+    end = ent.spans[0][-1] - span_start
 
     try:
-        arg1_pos = [t.pos_ for t in sentence.char_span(arg1_start, arg1_end)]
-    except:
+        arg1_pos = [t.pos_ for t in sentence.char_span(start, end)]
+    except TypeError:
+        # The char span doesn't line up with any tokens,
+        # thus we can't figure out if the prediction is the right POS
         return None
 
-    arg2_start = rel.arg2.spans[0][0] - span_start
-    arg2_end = rel.arg2.spans[0][-1] - span_start
-
-    arg1_masked = text[:arg1_start] + '[MASK]' + text[arg1_end:]
-    arg2_masked = text[:arg2_start] + '[MASK]' + text[arg2_end:]
-    # arg2_pos = [t.pos_ for t in sentence.char_span(arg2_start, arg2_end)]
-
-    arg1_tokenized = bert_tokenizer.tokenize(arg1_masked)
-    arg2_tokenized = bert_tokenizer.tokenize(arg2_masked)
-
-    arg1_indexed_tokens = bert_tokenizer.convert_tokens_to_ids(arg1_tokenized)
-    arg2_indexed_tokens = bert_tokenizer.convert_tokens_to_ids(arg2_tokenized)
-
-    arg1_token_tensor = torch.tensor(arg1_indexed_tokens)
-    arg2_token_tensor = torch.tensor(arg2_indexed_tokens)
-
-    arg1_mask_tensor = torch.tensor([token != '[MASK]' for token in arg1_tokenized], dtype=torch.float)
-    arg2_mask_tensor = torch.tensor([token != '[MASK]' for token in arg2_tokenized], dtype=torch.float)
+    masked_sentence = text[:start] + '[MASK]' + text[end:]
+    tokenized_sentence = bert_tokenizer.tokenize(masked_sentence)
+    indexed_tokens = bert_tokenizer.convert_tokens_to_ids(tokenized_sentence)
+    token_tensor = torch.tensor(indexed_tokens)
+    mask_tensor = torch.tensor([token != '[MASK]' for token in tokenized_sentence], dtype=torch.float)
 
     with torch.no_grad():
-        arg1_result = bert(arg1_token_tensor.unsqueeze(0), arg1_mask_tensor.unsqueeze(0), masked_lm_labels=None)
-        arg2_result = bert(arg2_token_tensor.unsqueeze(0), arg2_mask_tensor.unsqueeze(0), masked_lm_labels=None)
+        result = bert(token_tensor.unsqueeze(0), mask_tensor.unsqueeze(0), masked_lm_labels=None)
 
-    arg1_result = arg1_result[0].squeeze(0)
-    arg2_result = arg2_result[0].squeeze(0)
+    result = result[0].squeeze(0)
+    scores = torch.softmax(result, dim=-1)
+    mask_index = tokenized_sentence.index('[MASK]')
 
-    arg1_scores = torch.softmax(arg1_result, dim=-1)
-    arg2_scores = torch.softmax(arg2_result, dim=-1)
+    _, topk_indices = torch.topk(scores[mask_index, :], k, sorted=True)
+    topk_tokens = bert_tokenizer.convert_ids_to_tokens(topk_indices)
 
-    arg1_mask_index = arg1_tokenized.index('[MASK]')
-    arg2_mask_index = arg2_tokenized.index('[MASK]')
-
-    _, topk_indices = torch.topk(arg1_scores[arg1_mask_index, :], k, sorted=True)
-    arg1_topk_tokens = bert_tokenizer.convert_ids_to_tokens(topk_indices)
-
-    _, topk_indices = torch.topk(arg2_scores[arg2_mask_index, :], k, sorted=True)
-    arg2_topk_tokens = bert_tokenizer.convert_ids_to_tokens(topk_indices)
-
-    for token in arg1_topk_tokens:
-        new_sent = text[:arg1_start] + token + text[arg1_end:]
+    for token in topk_tokens:
+        new_sent = text[:start] + token + text[end:]
         new_doc = nlp(new_sent)
-        new_span = new_doc.char_span(arg1_start, arg1_start + len(token))
+        new_span = new_doc.char_span(start, start + len(token))
+
         if new_span is None:
-            print('none reject')
             continue
         if [t.pos_ for t in new_span] != arg1_pos:
-            print('reject')
             continue
 
-        print('accept!')
+        ent.spans = [(start, start + len(token))]
+        ent.mention = token
 
-        new_arg1 = copy(rel.arg1)
-        new_arg1.spans = [(arg1_start, arg1_start + len(token))]
-        new_arg1.mention = token
+        if do_left:
+            new_offset = len(token) - len(ent.mention)
+            other_ent.spans = [(other_ent.spans[0][0] + new_offset, other_ent.spans[-1][-1] + new_offset)]
 
-        new_offset = len(token) - len(rel.arg1.mention)
+        yield PseudoSentence(rel, new_sent)
 
-        new_arg2 = copy(rel.arg2)
-        new_arg2.spans = [(arg2_start + new_offset, arg2_end + new_offset)]
 
-        new_rel = brat_data.Relation(rel.relation, new_arg1, new_arg2)
-        yield PseudoSentence(new_rel, new_sent)
-
-    # for token in arg2_topk_tokens:
-    #     new_sent = text[:arg2_start] + token + text[arg2_end:]
-    #     new_arg2 = copy(rel.arg2)
-    #     new_arg2.spans = [(arg1_start, arg1_start + len(token))]
-    #     new_arg2.mention = token
-    #
-    #     new_offset = len(token) - len(rel.arg2.mention)
-    #
-    #     new_arg1 = copy(rel.arg1)
-    #     new_arg1.spans = [(arg2_start + new_offset, arg2_end + new_offset)]
-    #
-    #     new_rel = brat_data.Relation(rel.relation, new_arg1, new_arg2)
-    #     yield PseudoSentence(new_rel, new_sent)
+def pseudofy_relation(rel: brat_data.Relation, sentence: Span, k=3) -> SentenceGenerator:
+    yield from _pseudofy_side(rel, sentence, k, True)
+    yield from _pseudofy_side(rel, sentence, k, False)
 
 
 def pseudofy_file(ann: brat_data.BratFile) -> SentenceGenerator:
