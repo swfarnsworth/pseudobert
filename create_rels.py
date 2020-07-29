cdaa14ba318b698f81571182a5bf9999c62024fc
swfarnsworth <swfarnsworth@gmail.com>
2020-07-29 12:59:53 -0400

    Going to make this less repetitive

--- create_rels.py
+++ create_rels.py
@@ -1,8 +1,8 @@
+import argparse
 import typing as t
 from copy import copy
 from dataclasses import dataclass
-import argparse
-from collections import namedtuple
+from enum import Enum
 
 import spacy
 import torch
@@ -10,7 +10,8 @@ import transformers as tfs
 from bratlib import data as brat_data
 from spacy.tokens.span import Span
 
-nlp = spacy.load('en_core_web_sm')
+
+nlp = spacy.load('en_core_sci_lg')
 bert = tfs.BertForMaskedLM.from_pretrained('allenai/scibert_scivocab_uncased')
 bert_tokenizer = tfs.BertTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
 
@@ -21,6 +22,16 @@ class PseudoSentence:
     sent: str
 
 
+SentenceGenerator = t.Generator[PseudoSentence, None, None]
+
+
+def adjust_ent_span(ent: brat_data.Entity, offset: int) -> None:
+    start, end = ent.spans[0]
+    start += offset
+    end += offset
+    ent.spans = [(start, end)]
+
+
 def find_sentence(start: int, sentences: t.Dict[range, Span]):
     for r, s in sentences.items():
         if start in r:
@@ -28,18 +39,31 @@ def find_sentence(start: int, sentences: t.Dict[range, Span]):
     raise RuntimeError(f'start value {start} not in any range')
 
 
-def pseudofy_relation(rel: brat_data.Relation, sentence: Span, k=1) -> t.Generator[PseudoSentence, None, None]:
+_Side = Enum('RIGHT LEFT')
+
+def _pseudofy_side(rel: brat_data.Relation, side: _Side):
+    ent = rel.arg1 if side is _Side.RIGHT else rel.arg2
+
+
+
+def pseudofy_relation(rel: brat_data.Relation, sentence: Span, k=3) -> SentenceGenerator:
     text = str(sentence)
     span_start = sentence.start_char
 
     arg1_start = rel.arg1.spans[0][0] - span_start
     arg1_end = rel.arg1.spans[0][-1] - span_start
 
+    try:
+        arg1_pos = [t.pos_ for t in sentence.char_span(arg1_start, arg1_end)]
+    except:
+        return None
+
     arg2_start = rel.arg2.spans[0][0] - span_start
     arg2_end = rel.arg2.spans[0][-1] - span_start
 
     arg1_masked = text[:arg1_start] + '[MASK]' + text[arg1_end:]
     arg2_masked = text[:arg2_start] + '[MASK]' + text[arg2_end:]
+    # arg2_pos = [t.pos_ for t in sentence.char_span(arg2_start, arg2_end)]
 
     arg1_tokenized = bert_tokenizer.tokenize(arg1_masked)
     arg2_tokenized = bert_tokenizer.tokenize(arg2_masked)
@@ -74,6 +98,17 @@ def pseudofy_relation(rel: brat_data.Relation, sentence: Span, k=1) -> t.Generat
 
     for token in arg1_topk_tokens:
         new_sent = text[:arg1_start] + token + text[arg1_end:]
+        new_doc = nlp(new_sent)
+        new_span = new_doc.char_span(arg1_start, arg1_start + len(token))
+        if new_span is None:
+            print('none reject')
+            continue
+        if [t.pos_ for t in new_span] != arg1_pos:
+            print('reject')
+            continue
+
+        print('accept!')
+
         new_arg1 = copy(rel.arg1)
         new_arg1.spans = [(arg1_start, arg1_start + len(token))]
         new_arg1.mention = token
@@ -86,44 +121,40 @@ def pseudofy_relation(rel: brat_data.Relation, sentence: Span, k=1) -> t.Generat
         new_rel = brat_data.Relation(rel.relation, new_arg1, new_arg2)
         yield PseudoSentence(new_rel, new_sent)
 
-    for token in arg2_topk_tokens:
-        new_sent = text[:arg2_start] + token + text[arg2_end:]
-        new_arg2 = copy(rel.arg2)
-        new_arg2.spans = [(arg1_start, arg1_start + len(token))]
-        new_arg2.mention = token
+    # for token in arg2_topk_tokens:
+    #     new_sent = text[:arg2_start] + token + text[arg2_end:]
+    #     new_arg2 = copy(rel.arg2)
+    #     new_arg2.spans = [(arg1_start, arg1_start + len(token))]
+    #     new_arg2.mention = token
+    #
+    #     new_offset = len(token) - len(rel.arg2.mention)
+    #
+    #     new_arg1 = copy(rel.arg1)
+    #     new_arg1.spans = [(arg2_start + new_offset, arg2_end + new_offset)]
+    #
+    #     new_rel = brat_data.Relation(rel.relation, new_arg1, new_arg2)
+    #     yield PseudoSentence(new_rel, new_sent)
 
-        new_offset = len(token) - len(rel.arg2.mention)
 
-        new_arg1 = copy(rel.arg1)
-        new_arg1.spans = [(arg2_start + new_offset, arg2_end + new_offset)]
-
-        new_rel = brat_data.Relation(rel.relation, new_arg1, new_arg2)
-        yield PseudoSentence(new_rel, new_sent)
-
-
-
-def pseudofy_file(ann: brat_data.BratFile) -> t.Generator[PseudoSentence, None, None]:
+def pseudofy_file(ann: brat_data.BratFile) -> SentenceGenerator:
     with ann.txt_path.open() as f:
         doc = nlp(f.read())
-    sentences = {range(sent.start_char, sent.end_char): sent for sent in doc.sents}
+    sentences = {range(sent.start_char, sent.end_char + 1): sent for sent in doc.sents}
 
     for rel in ann.relations:
         # Make sure all entities are in the same sentence
         first_ent_start, first_ent_end = rel.arg1.spans[0][0], rel.arg1.spans[-1][-1]
         last_ent_start, last_ent_end = rel.arg2.spans[0][0], rel.arg2.spans[-1][-1]
 
-        try:
-            ent1_sent_a = find_sentence(first_ent_start, sentences)
-            ent1_sent_b = find_sentence(first_ent_end, sentences)
-            ent2_sent_a = find_sentence(last_ent_start, sentences)
-            ent2_sent_b = find_sentence(last_ent_end, sentences)
-        except RuntimeError:
-            continue
+        ent1_sent_a = find_sentence(first_ent_start, sentences)
+        ent1_sent_b = find_sentence(first_ent_end, sentences)
+        ent2_sent_a = find_sentence(last_ent_start, sentences)
+        ent2_sent_b = find_sentence(last_ent_end, sentences)
 
         if not (ent1_sent_a is ent1_sent_b is ent2_sent_a is ent2_sent_b):
             continue
 
-        yield from pseudofy_relation(rel, ent1_sent_a)
+        yield from filter(None, pseudofy_relation(rel, ent1_sent_a))
 
 
 def main():
