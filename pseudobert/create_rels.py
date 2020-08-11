3150c6ee9d1e066a1539265bea0bf516d985c073
swfarnsworth <swfarnsworth@gmail.com>
2020-08-10 22:09:54 -0400

    Converted to installable library

--- pseudobert/create_rels.py
+++ pseudobert/create_rels.py
@@ -205,22 +205,3 @@ class PseudoBertRelator:
         for ann in dataset:
             self._psudofy_file(ann, output_dir)
         return brat_data.BratDataset.from_directory(output_dir)
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_dataset')
-    parser.add_argument('output_directory')
-    args = parser.parse_args()
-
-    dataset = brat_data.BratDataset.from_directory(args.input_dataset)
-    output_dir = Path(args.output_directory)
-
-    logging.basicConfig(filename=output_dir / 'pseudobert.log', level=logging.INFO)
-
-    pbr = PseudoBertRelator.init_scientific()
-    pbr.pseudofy_dataset(dataset, output_dir)
-
-
-if __name__ == '__main__':
-    main()
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pseudobert',
+    packages=find_packages(),
+    version='0.0.0',
+    description='Pseudo data generation with BERT',
+    author='Steele Farnsworth',
+    install_requires=[
+        'torch',
+        'transformers',
+        'scispacy',
+        'spacy',
+        'git+https://github.com/swfarnsworth/bratlib.git',
+        'https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.5/en_core_sci_lg-0.2.5.tar.gz'
+    ]
+)
